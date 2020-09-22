739304f011e590d29a4bd7f5c8b22224c3ee7d51
gl-guccione <g.guccione3@gmail.com>
2020-09-22 15:47:50 +0200

    Milestone1 - js

--- js/script.js
+++ js/script.js
@@ -9,3 +9,58 @@
 // Titolo Originale
 // Lingua
 // Voto
+
+// functions
+
+// function that read the value of the #search input and make an ajax request
+function displayFilm() {
+
+  var searchValue = $("#search").val();
+
+  if (searchValue != "") {
+    $.ajax(
+      {
+        "url": "https://api.themoviedb.org/3/search/movie",
+        "data": {
+          "api_key": "bde17cab6f3147255ce4802924edb786",
+          "language": "it-IT",
+          "include_adult": "false",
+          "query": searchValue,
+        },
+        "method": "GET",
+        "success": function(date, state) {
+          printFilms(date.results);
+        },
+        "error": function() {
+          alert("error");
+        },
+      }
+    );
+  }
+}
+
+// function that take the results of the ajax request and print the list of the films with handlebars
+function printFilms(object) {
+
+  var source = $("#films-template").html();
+  var template = Handlebars.compile(source);
+
+  $(".films").html("");
+
+  for (var i = 0; i < object.length; i++) {
+    var html = template(object[i]);
+    $(".films").append(html);
+  }
+
+}
+
+// script
+
+// document ready
+$(document).ready(function() {
+
+  $("#search").val("");
+
+  $(".searchbar__button").click(displayFilm);
+
+});

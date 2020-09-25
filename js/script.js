1d28da3dfdcbb79ae8f2bd6098283e52c3c0c6e5
gl-guccione <g.guccione3@gmail.com>
2020-09-25 12:32:51 +0200

    Refactoring

--- js/script.js
+++ js/script.js
@@ -1,18 +1,21 @@
-// Milestone 1
-
-// Creare un layout base con una searchbar (una input e un button)
-// in cui possiamo scrivere completamente o parzialmente il nome di un film.
-// Possiamo, cliccando il  bottone, cercare sull’API tutti i film che contengono ciò che ha scritto l’utente.
-
-// Vogliamo dopo la risposta dell’API visualizzare a schermo i seguenti valori per ogni film trovato:
-// Titolo
-// Titolo Originale
-// Lingua
-// Voto
-
 // functions
 
-// function that convert a (number) in base 10 to a number in base 5
+function searchContent() {
+  var searchValue = $("#search").val();
+  clear();
+  requestData(searchValue, "movie");
+  requestData(searchValue, "tv");
+
+  $("#search").val("");
+}
+
+// function that clear the film and tv <ul>
+function clear() {
+  $(".films").html("");
+  $(".tv-shows").html("");
+}
+
+// function that convert a decimal (number) to a number in base 5
 function toBaseFive(number) {
   if (!isNaN(number)) {
     return Math.ceil(number / 2);
@@ -21,24 +24,31 @@ function toBaseFive(number) {
   }
 }
 
-// function that read the value of the #search input and make an ajax request for the tv-shows
-function displayTvShows() {
+// function that read the value of the #search input and make an ajax request for (type)
+function requestData(search, type) {
 
-  var searchValue = $("#search").val();
+  if (type == "movie") {
+    var httpBody = "/search/movie";
+  } else if (type == "tv") {
+    var httpBody = "/search/tv";
+  }
 
-  if (searchValue != "") {
+  var uri = "https://api.themoviedb.org/3";
+  var url = uri + httpBody;
+
+  if (search != "") {
     $.ajax(
       {
-        "url": "https://api.themoviedb.org/3/search/tv",
+        "url": url,
         "data": {
           "api_key": "bde17cab6f3147255ce4802924edb786",
           "language": "it-IT",
           "include_adult": "false",
-          "query": searchValue,
+          "query": search,
         },
         "method": "GET",
         "success": function(date, state) {
-          printTvShows(date.results);
+          printData(type, date.results);
         },
         "error": function() {
           alert("error");
@@ -48,53 +58,18 @@ function displayTvShows() {
   }
 }
 
-// function that take the results of the ajax request and print the list of the tv-shows with handlebars
-function printTvShows(arrObject) {
+// function that take the results of the ajax request and print the list of (type) with handlebars
+function printData(type, arrObject) {
 
-  var source = $("#tv-shows-template").html();
-  var template = Handlebars.compile(source);
-
-  for (var i = 0; i < arrObject.length; i++) {
-
-    arrObject[i]["stars"] = toBaseFive(arrObject[i].vote_average);
-
-    var html = template(arrObject[i]);
-    $(".tv-shows").append(html);
+  if (type == "movie") {
+    var htmlTemplate = $("#films-template");
+    var dataContainer = $(".films");
+  } else if (type == "tv") {
+    var htmlTemplate = $("#tv-shows-template");
+    var dataContainer = $(".tv-shows");
   }
 
-}
-
-// function that read the value of the #search input and make an ajax request for the films
-function displayFilm() {
-
-  var searchValue = $("#search").val();
-
-  if (searchValue != "") {
-    $.ajax(
-      {
-        "url": "https://api.themoviedb.org/3/search/movie",
-        "data": {
-          "api_key": "bde17cab6f3147255ce4802924edb786",
-          "language": "it-IT",
-          "include_adult": "false",
-          "query": searchValue,
-        },
-        "method": "GET",
-        "success": function(date, state) {
-          printFilms(date.results);
-        },
-        "error": function() {
-          alert("error");
-        },
-      }
-    );
-  }
-}
-
-// function that take the results of the ajax request and print the list of the films with handlebars
-function printFilms(arrObject) {
-
-  var source = $("#films-template").html();
+  var source = htmlTemplate.html();
   var template = Handlebars.compile(source);
 
   for (var i = 0; i < arrObject.length; i++) {
@@ -106,7 +81,7 @@ function printFilms(arrObject) {
     arrObject[i]["stars"] = toBaseFive(arrObject[i].vote_average);
 
     var html = template(arrObject[i]);
-    $(".films").append(html);
+    dataContainer.append(html);
   }
 
 }
@@ -119,18 +94,12 @@ $(document).ready(function() {
   $("#search").val("");
 
   $(".searchbar__button").click(function() {
-    $(".films").html("");
-    displayFilm();
-    displayTvShows()
-    $("#search").val("");
+    searchContent();
   });
 
   $("#search").keyup(function(e) {
     if (e.which == 13 && $("#search").val() != "") {
-      $(".films").html("");
-      displayFilm();
-      displayTvShows()
-      $("#search").val("");
+      searchContent();
     }
   });
 
